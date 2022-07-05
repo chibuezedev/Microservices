6d6efab920dd2e8e5bffe0829c2bd783efa67462
Paul Chibueze <paulpadro03@gmail.com>
2022-07-05 16:03:09 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=cdr
+export POSTGRES_PASSWORD=1234567890Pp
+export POSTGRES_HOST=cdr.cxekqlnpprvv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::chibuezebucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
