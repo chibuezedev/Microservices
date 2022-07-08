171ca44751be65a0ea696a5f339f02a592f9416c
Paul Chibueze <paulpadro03@gmail.com>
2022-07-09 00:54:26 +0100

    commit

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=cdr
 export POSTGRES_PASSWORD=1234567890Pp
-export POSTGRES_HOST=cdr.cxekqlnpprvv.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=cdr.cyocr0vzi7wo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::chibuezebucket
+export AWS_BUCKET=arn:aws:s3:::pcdevbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
